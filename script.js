eb49284c3ef21a3757d8cd5ae9de53402ac41ab1
realsimile <realsimile@gmail.com>
2024-08-08 19:54:05 +0400

    aaa

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let MoneyButton = document.getElementById("MoneyButton");
+
+    MoneyButton.addEventListener("click", function(){
+        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    })
+});

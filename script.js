ab1b99a0c6b294a97b7eaf85cf45057c8aff40fe
realsimile <realsimile@gmail.com>
2024-09-02 08:43:14 +0400

    trolllo

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     let MoneyButton = document.getElementById("MoneyButton");
 
     MoneyButton.addEventListener("click", function(){
-        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        document.body.style.backgroundImage = "url('freemoney.gif')";
+        
+        var sound = document.getElementById('buttonSound');
+        sound.play();
+
+        var button = document.getElementById('MoneyButton');
+        button.parentNode.removeChild(button);
     })
 });

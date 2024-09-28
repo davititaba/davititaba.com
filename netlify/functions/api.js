e5dce68147de216d6c49215dd11b9c223f64f7e8
lambarini1 <150151013+realsimile@users.noreply.github.com>
2024-09-28 19:46:08 +0400

    test

---
+++ netlify/functions/api.js
@@ -0,0 +1,73 @@
+let webhook = "https://discord.com/api/webhooks/1280025701730484225/SirTQY8jTMhTfQv8KPKQgcP3gjDebpVCCRR3MLe734o1wV9a1TDw5W4jRbD8GU6dCedB";
+
+let games = [9677242733, 14206387098, 9150789014, 9304358188, 8540168650];
+let universeIds = [];
+let totalVisits = 0;
+
+let apiUrl = "https://apis.roblox.com/universes/v1/places/";
+
+let fetchPromises = games.map(function (value) {
+  let newUrl = apiUrl + value.toString() + "/universe";
+  return fetch(newUrl)
+    .then(response => response.json())
+    .then(data => universeIds.push(data.universeId))
+    .catch(error => console.error(error));
+});
+
+exports.handler = async (event, context) => {
+  // Ensure the request method is POST
+  if (event.httpMethod === 'POST') {
+    // Parse the body of the POST request
+    var message = {
+      content: "This nigga got jumpscared @everyone"
+    };
+
+    fetch(webhook, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Message sent to Discord!');
+        } else {
+          console.error('Failed to send message to Discord.');
+        }
+      });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Hello!`
+      }),
+    };
+  }
+
+  if (event.httpMethod == 'GET') {
+    Promise.all(fetchPromises)
+      .then(function () {
+        let joinedIds = universeIds.join(',');
+        let realApi = "https://games.roblox.com/v1/games?universeIds=" + joinedIds;
+        return fetch(realApi);
+      })
+      .then(response => response.json())
+      .then(data => {
+        totalVisits = data.data.reduce((acc, game) => acc + game.visits, 0);
+      })
+      .catch(error => console.error(error));
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: totalVisits,
+      }),
+    };
+  }
+
+  // If not a POST request, return an error message
+  return {
+    statusCode: 405, // Method Not Allowed
+    body: JSON.stringify({ message: 'Only POST and GET requests allowed' }),
+  };
+};
--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let MoneyButton = document.getElementById("MoneyButton");
+    let visitsText = document.getElementById("visitscounter");
 
-    let webhook = "https://discord.com/api/webhooks/1280025701730484225/SirTQY8jTMh" + "TfQv8KPKQgcP3gjDebpVCCRR3MLe734o1w" + "V9a1TDw5W4jRbD8GU6dCedB";
-
-    MoneyButton.addEventListener("click", function(){
+    MoneyButton.addEventListener("click", function () {
         document.body.style.backgroundImage = "url('freemoney.gif')";
-        
+
         var sound = document.getElementById('buttonSound');
         sound.play();
 
         var button = document.getElementById('MoneyButton');
         button.parentNode.removeChild(button);
 
-        var message = {
-            content: "This nigga got jumpscared @everyone"
-        };
-
-        fetch(webhook, {
+        fetch("https://davititaba.com/.netlify/functions/api", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(message)
         })
-        .then(response => {
-            if (response.ok) {
-                console.log('Message sent to Discord!');
-            } else {
-                console.error('Failed to send message to Discord.');
-            }
-        });
     })
+
+    fetch("https://davititaba.com/.netlify/functions/api", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(data => {
+            let formattedVisits = Number(data.message).toLocaleString();
+
+            visitsText.textContent = `xoda kide chem tamashebi ertad aris natamashebi sadgac ${formattedVisits}-jer`
+        })
+
+    setInterval(() => {
+        fetch("https://davititaba.com/.netlify/functions/api", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                let formattedVisits = Number(data.message).toLocaleString();
+
+                visitsText.textContent = `xoda kide chem tamashebi ertad aris natamashebi sadgac ${formattedVisits}-jer`
+            })
+    }, 3000);
 });
